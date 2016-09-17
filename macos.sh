d151310be74cbf5a13fa45340c9b87897ddc2d36
Shun Sato <ponta.konta27@gmail.com>
2016-09-17 22:03:35 +0900

    chore(macos.sh): fix terminal bell setting

--- macos.sh
+++ macos.sh
@@ -676,7 +676,7 @@ EOD
 TERMINAL_PLIST="$HOME/Library/Preferences/com.apple.Terminal.plist"
 TERMINAL_THEME=`/usr/libexec/PlistBuddy -c "Print 'Default Window Settings'" $TERMINAL_PLIST`
 /usr/libexec/PlistBuddy -c "Set 'Window Settings':$TERMINAL_THEME:Bell false" $TERMINAL_PLIST
-/usr/libexec/PlistBuddy -c "Set 'Window Settings':$TERMINAL_THEME:VisualBellOnlyWhenMuted false" $TERMINAL_PLIST
+/usr/libexec/PlistBuddy -c "Set 'Window Settings':$TERMINAL_THEME:VisualBell false" $TERMINAL_PLIST
 
 # Enable “focus follows mouse” for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it without clicking first

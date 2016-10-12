ee36bd3a6fad197f60c94cb113418b2bf40754b8
Shun Sato <ponta.konta27@gmail.com>
2016-10-12 23:25:37 +0900

    chore(bootstrap.sh): exclude .dots from rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ git pull origin master;
 
 function doIt() {
   rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-    --exclude "README.md" --exclude "LICENSE" --exclude "*.sh" -avh --no-perms . ~;
+    --exclude "README.md" --exclude "LICENSE" --exclude "*.sh" --exclude ".dots" -avh --no-perms . ~;
   source ~/.bash_profile;
 }
 

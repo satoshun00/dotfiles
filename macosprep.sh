bf7719243ba6c27ef8f70c08628e7247f00dafd3
satoshun00 <shun.sato@fenneclab.com>
2023-06-18 04:01:33 +0900

    minor update

--- macosprep.sh
+++ macosprep.sh
@@ -24,10 +24,11 @@ echo "Installing Xcode Command Line Tools."
 xcode-select -p &> /dev/null
 if [ $? -ne 0 ]; then
   echo "Command Line Tools for Xcode not found. Installing from softwareupdate…"
-# This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+  # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
   touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
   PROD=$(softwareupdate -l | grep "\*.*Command Line" | tail -n 1 | sed 's/^[^C]* //')
   softwareupdate -i "$PROD" --verbose;
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
 else
   echo "Command Line Tools for Xcode have been installed."
 fi

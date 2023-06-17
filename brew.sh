084485ef0d28f4f6e1425bb18645d97e588ab94d
satoshun00 <shun.sato@fenneclab.com>
2023-06-18 03:49:22 +0900

    fix brew install

--- brew.sh
+++ brew.sh
@@ -15,6 +15,9 @@ if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> ~/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Make sure we’re using the latest Homebrew.
 brew update
 
